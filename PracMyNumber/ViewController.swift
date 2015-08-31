85f6b9872794bbf8cfe7ce3eb1c2519fbb3efaab
Tanin Rojanapiansatith <sp_land@hotmail.com>
2015-08-31 13:22:50 +0700

    Initial Commit

---
+++ PracMyNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracMyNumber
+//
+//  Created by landtanin on 8/31/2558 BE.
+//  Copyright (c) 2558 landtanin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
